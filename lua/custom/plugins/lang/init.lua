15b3e2ca7836f1d38868c77c3cf3455d72553a8b
Mike Flood <flood.mike@gmail.com>
2024-04-12 10:59:45 -0500

    Add Java support

--- lua/custom/plugins/lang/init.lua
+++ lua/custom/plugins/lang/init.lua
@@ -1,4 +1,5 @@
 return {
+  { import = 'custom.plugins.lang.java' },
   { import = 'custom.plugins.lang.nix' },
   { import = 'custom.plugins.lang.scala' },
   { import = 'custom.plugins.lang.terraform' },
