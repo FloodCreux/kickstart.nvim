6a7053d8c6a89a27c32e660157c3b49772e6ee0b
Mike Flood <flood.mike@gmail.com>
2024-04-12 11:00:35 -0500

    add dap support for java

--- lua/custom/plugins/lang/java/init.lua
+++ lua/custom/plugins/lang/java/init.lua
@@ -27,5 +27,18 @@ return {
       },
     },
   },
+  {
+    'mfussenegger/nvim-dap',
+    optional = true,
+    dependencies = {
+      {
+        'williamboman/mason.nvim',
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { 'java-test', 'java-debug-adapter' })
+        end,
+      },
+    },
+  },
   { import = 'custom.plugins.lang.java.jdtls' },
 }
