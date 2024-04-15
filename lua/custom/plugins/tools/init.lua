67012c0bc9cf236426917a01cb3c0f9ddd112a4a
FloodCreux <flood.mike@gmail.com>
2024-04-15 10:06:33 -0500

    add cronex

--- lua/custom/plugins/tools/init.lua
+++ lua/custom/plugins/tools/init.lua
@@ -1,5 +1,6 @@
 return {
   { import = 'custom.plugins.tools.codesnap' },
+  { import = 'custom.plugins.tools.cronex' },
   { import = 'custom.plugins.tools.harpoon' },
   { import = 'custom.plugins.tools.lazygit' },
 }
