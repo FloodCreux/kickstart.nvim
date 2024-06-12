ee1a29824a32a039ee8282b5a40c0fc7c1e5090f
FloodCreux <flood.mike@gmail.com>
2024-06-12 13:52:19 -0500

    cleanup

--- lua/custom/plugins/editor/conform.lua
+++ lua/custom/plugins/editor/conform.lua
@@ -30,10 +30,7 @@ return {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettier' } },
         cs = { 'csharpier' },
         xml = { 'xmlformat' },
         -- sql = { 'sqlfmt' },
--- lua/custom/plugins/tools/codesnap.lua
+++ lua/custom/plugins/tools/codesnap.lua
@@ -2,10 +2,17 @@ return {
   {
     'mistricky/codesnap.nvim',
     build = 'make',
+    keys = {
+      { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
+      { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
+    },
     config = function()
       require('codesnap').setup {
         watermark = '',
       }
     end,
+    opts = {
+      save_path = '~/Pictures',
+    },
   },
 }
