12d6f85586f10efe291bd5917bf8d97f1aec8cca
FloodCreux <flood.mike@gmail.com>
2024-06-12 14:17:44 -0500

    re-arranging

--- lua/custom/plugins/editor/init.lua
+++ lua/custom/plugins/editor/init.lua
@@ -1,4 +1,7 @@
 return {
-  { import = 'custom.plugins.editor.trouble' },
   { import = 'custom.plugins.editor.conform' },
+  { import = 'custom.plugins.editor.lsp' },
+  { import = 'custom.plugins.editor.telescope' },
+  { import = 'custom.plugins.editor.treesitter' },
+  { import = 'custom.plugins.editor.trouble' },
 }
--- lua/custom/plugins/tools/init.lua
+++ lua/custom/plugins/tools/init.lua
@@ -1,9 +1,12 @@
 return {
   { import = 'custom.plugins.tools.codesnap' },
+  { import = 'custom.plugins.tools.comments' },
   { import = 'custom.plugins.tools.completion' },
   { import = 'custom.plugins.tools.cronex' },
+  { import = 'custom.plugins.tools.gitsigns' },
   { import = 'custom.plugins.tools.harpoon' },
   { import = 'custom.plugins.tools.lazygit' },
+  { import = 'custom.plugins.tools.mini' },
   { import = 'custom.plugins.tools.oil' },
   { import = 'custom.plugins.tools.wakatime' },
 }
