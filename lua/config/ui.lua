ac6e6651df2967440e90333e740748f3ac817b67
FloodCreux <flood.mike@gmail.com>
2024-06-12 14:20:49 -0500

    add ui file

---
+++ lua/config/ui.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.icons = {
+  cmd = '⌘',
+  config = '🛠',
+  event = '📅',
+  ft = '📂',
+  init = '⚙',
+  keys = '🗝',
+  plugin = '🔌',
+  runtime = '💻',
+  require = '🌙',
+  source = '📄',
+  start = '🚀',
+  task = '📌',
+  lazy = '💤 ',
+}
+
+M.borders = 'rounded'
+
+return M
