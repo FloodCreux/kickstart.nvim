62ed81545edb1239611549c5f67f3ec4768151fd
FloodCreux <flood.mike@gmail.com>
2024-07-22 09:14:32 -0500

    add markview

--- lua/custom/plugins/lang/markdown/markview.lua
+++ lua/custom/plugins/lang/markdown/markview.lua
@@ -1,3 +1,15 @@
+-- local add = require('mini.deps').add
+--
+-- add {
+--   source = 'OXY2DEV/markview.nvim',
+--   depends = {
+--     'nvim-treesitter/nvim-treesitter',
+--     'nvim-tree/nvim-web-devicons',
+--   },
+-- }
+--
+-- require('markview').setup()
+
 return {
   'OXY2DEV/markview.nvim',
   dependencies = {
